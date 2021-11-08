d6dfeafc0b9683c87309535d2b06005ba9aec11b
Tanawat Yuwansiri <TY_Sage@hotmail.com>
2021-11-08 19:22:16 +0700

    change: Padding to Margin

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -29,27 +29,25 @@ class HomePage extends StatelessWidget {
         children: [
           for(int i=0;i<2;i++)
             Expanded(
-              child: Padding(
-                padding: const EdgeInsets.only(right: 16.0),
-                child: Card(
-                  child: InkWell(
-                    onTap: (){
-                      Navigator.pushNamed(
-                        context,
-                        pageInfo[i]['routeName'],
-                      );
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        children: [
-                          Icon(pageInfo[i]['icon'], size: 80.0),
-                          Text(
-                            pageInfo[i]['title'],
-                            style: Theme.of(context).textTheme.bodyText1,
-                          ),
-                        ],
-                      ),
+              child: Card(
+                margin: const EdgeInsets.only(right: 16.0),
+                child: InkWell(
+                  onTap: (){
+                    Navigator.pushNamed(
+                      context,
+                      pageInfo[i]['routeName'],
+                    );
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Icon(pageInfo[i]['icon'], size: 80.0),
+                        Text(
+                          pageInfo[i]['title'],
+                          style: Theme.of(context).textTheme.bodyText1,
+                        ),
+                      ],
                     ),
                   ),
                 ),
