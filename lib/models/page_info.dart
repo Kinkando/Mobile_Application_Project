ae310f92933872a775d24b837dc82cf57a0efc17
Tanawat Yuwansiri <TY_Sage@hotmail.com>
2021-11-11 01:35:02 +0700

    add: missing route name of search anime page

--- lib/models/page_info.dart
+++ lib/models/page_info.dart
@@ -20,7 +20,7 @@ final Map<String, PageInfo> pageList = {
   'seasonal_anime': PageInfo(title: 'Seasonal Anime', routeName: '/anime/season', page: 1, endPoint: 'season', icon: Icons.cloud),
   'schedule_anime': PageInfo(title: 'Schedule Anime', routeName: '/anime/schedule', page: 2, endPoint: 'schedule', icon: Icons.date_range),
   'top_anime': PageInfo(title: 'Top Anime', routeName: '/anime/top', page: 3, endPoint: 'top/anime', icon: Icons.whatshot),
-  'search_anime': PageInfo(title: 'Search Anime', routeName: 'anime/search', page: 4, endPoint: 'genre/anime', icon: Icons.search),
+  'search_anime': PageInfo(title: 'Search Anime', routeName: '/anime/search', page: 4, endPoint: 'genre/anime', icon: Icons.search),
   'top_manga': PageInfo(title: 'Top Manga', routeName: '/manga/top', page: 5, endPoint: 'top/manga', icon: Icons.whatshot),
   'search_manga': PageInfo(title: 'Search Manga', routeName: '/manga/search', page: 6, endPoint: 'genre/manga', icon: Icons.search),
   'detail_anime': PageInfo(title: 'Anime Detail', routeName: '/anime/detail'),
