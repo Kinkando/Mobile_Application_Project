33bda27f5e363b1665ea2b9642e761659eb4da11
Tanawat Yuwansiri <TY_Sage@hotmail.com>
2021-11-10 10:18:55 +0700

    remove: unnecessary file

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -15,9 +15,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "4e525bf38f01ee1482ac74518bb83368",
-"/": "4e525bf38f01ee1482ac74518bb83368",
-"main.dart.js": "da22584c85e6150d1749c0b698d05241",
+"index.html": "3ac0def138a513885c1f983d04b9a464",
+"/": "3ac0def138a513885c1f983d04b9a464",
+"main.dart.js": "98301e7d89ff8db251cb2dd8375bf30a",
 "manifest.json": "0b637ed19ebbd9f9a51ea484547de9f6",
 "version.json": "626f1e97171be407a60f9d5be63ad22d"
 };
