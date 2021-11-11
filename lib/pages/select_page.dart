d4590bc46c3fdb8f38d1421c62732a69006b0597
Tanawat Yuwansiri <TY_Sage@hotmail.com>
2021-11-11 14:31:44 +0700

    add: scroll controller

--- lib/pages/select_page.dart
+++ lib/pages/select_page.dart
@@ -1,13 +1,14 @@
 import 'package:anime_list_project/models/anime.dart';
+import 'package:anime_list_project/pages/home_page.dart';
 import 'package:anime_list_project/pages/widgets/build_card.dart';
 import 'package:anime_list_project/pages/widgets/build_topic_card.dart';
-import 'package:anime_list_project/pages/widgets/main_scaffold.dart';
 import 'package:anime_list_project/utils/fetch_future.dart';
 import 'package:anime_list_project/models/page_info.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class SelectPage extends StatefulWidget {
+  static bool check = false;
   final PageInfo pageDetail;
   const SelectPage({Key? key, required this.pageDetail}) : super(key: key);
 
@@ -27,7 +28,6 @@ class _SelectPageState extends State<SelectPage> {
     ["Sunday", Colors.red],
   ];
   int _page = 1;
-  final _scheduleScrollController = ScrollController();
   final _futureScrollController = ScrollController();
 
   @override
@@ -40,73 +40,84 @@ class _SelectPageState extends State<SelectPage> {
     );
   }
 
+  void _initial() {
+    setState(() {
+      if(SelectPage.check) {
+        _page = 1;
+        _futureList = fetchApi(
+          widget.pageDetail.endPoint!.contains('top')
+              ? '${widget.pageDetail.endPoint!}/$_page'
+              : widget.pageDetail.endPoint!,
+        );
+        SelectPage.check = false;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MainScaffold(
-      title: widget.pageDetail.title,
-      page: widget.pageDetail.page!,
-      body: FutureBuilder<dynamic>(
-        future: _futureList,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState != ConnectionState.done) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
+    _initial();
+    return FutureBuilder<dynamic>(
+      future: _futureList,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState != ConnectionState.done) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
 
-          if (snapshot.hasData) {
-            var list = snapshot.data;
-            return ListView.builder(
-              shrinkWrap: widget.pageDetail.endPoint!.contains(pageList['search_anime']!.endPoint!),
-              padding: const EdgeInsets.all(8.0),
-              controller: _futureScrollController,
-              itemCount: list.length,
-              itemBuilder: (BuildContext context, int index) {
-                if(widget.pageDetail.endPoint == pageList['schedule_anime']!.endPoint) {
-                  return _buildScheduleCard(context, list[index], _weekStyle[index]);
-                }
-                return index == list.length-1 && widget.pageDetail.endPoint!.contains('top')
-                    ? _buildTopRankingCard(context, list[index])
-                    : BuildCard(item: list[index], endPoint: widget.pageDetail.endPoint!);
-              },
-            );
-          }
+        if (snapshot.hasData) {
+          var list = snapshot.data;
+          return ListView.builder(
+            shrinkWrap: widget.pageDetail.endPoint!.contains(pageList['search_anime']!.endPoint!),
+            controller: _futureScrollController,
+            padding: const EdgeInsets.all(8.0),
+            itemCount: list.length,
+            itemBuilder: (BuildContext context, int index) {
+              if(widget.pageDetail.endPoint == pageList['schedule_anime']!.endPoint) {
+                return _buildScheduleCard(context, list[index], _weekStyle[index]);
+              }
+              return index == list.length-1 && widget.pageDetail.endPoint!.contains('top')
+                  ? _buildTopRankingCard(context, list[index])
+                  : BuildCard(item: list[index], endPoint: widget.pageDetail.endPoint!);
+            },
+          );
+        }
 
-          if (snapshot.hasError) {
-            return Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Error: ${snapshot.error}',
-                    style: GoogleFonts.notoSans(color: MainScaffold.defaultColor),
+        if (snapshot.hasError) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Error: ${snapshot.error}',
+                  style: GoogleFonts.notoSans(color: HomePage.defaultColor),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _futureList = fetchApi(
+                        widget.pageDetail.endPoint!.contains('top')
+                            ? '${widget.pageDetail.endPoint!}/$_page'
+                            : widget.pageDetail.endPoint!,
+                      );
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: HomePage.backgroundColor,
                   ),
-                  ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        _futureList = fetchApi(
-                          widget.pageDetail.endPoint!.contains('top')
-                              ? '${widget.pageDetail.endPoint!}/$_page'
-                              : widget.pageDetail.endPoint!,
-                        );
-                      });
-                    },
-                    style: ElevatedButton.styleFrom(
-                      primary: MainScaffold.backgroundColor,
-                    ),
-                    child: Text(
-                      'Try again!',
-                      style: GoogleFonts.notoSans(color: MainScaffold.defaultColor),
-                    ),
+                  child: Text(
+                    'Try again!',
+                    style: GoogleFonts.notoSans(color: HomePage.defaultColor),
                   ),
-                ],
-              ),
-            );
-          }
+                ),
+              ],
+            ),
+          );
+        }
 
-          return const SizedBox.shrink();
-        },
-      ),
+        return const SizedBox.shrink();
+      },
     );
   }
 
@@ -131,11 +142,11 @@ class _SelectPageState extends State<SelectPage> {
                   });
                 },
                 style: ElevatedButton.styleFrom(
-                  primary: MainScaffold.backgroundColor,
+                  primary: HomePage.backgroundColor,
                 ),
                 child: Text(
                   _page>1 ? 'Back' : 'Next',
-                  style: GoogleFonts.notoSans(color: MainScaffold.defaultColor),
+                  style: GoogleFonts.notoSans(color: HomePage.defaultColor),
                 ),
               ),
             ),
@@ -146,25 +157,19 @@ class _SelectPageState extends State<SelectPage> {
   }
 
   Widget _buildScheduleCard(BuildContext context, List<Anime> item, List weekStyle) {
-    return ListView.builder(
-        shrinkWrap: true,
-        padding: const EdgeInsets.all(8.0),
-        controller: _scheduleScrollController,
-        itemCount: item.length,
-        itemBuilder: (BuildContext context, int index) {
-          return index != 0
-              ? BuildCard(item: item[index], endPoint: widget.pageDetail.endPoint!)
-              : Column(
-            children: [
-              BuildTopicCard(
-                topic: weekStyle[0],
-                color: weekStyle[1],
-                margin: const EdgeInsets.all(8.0),
-              ),
-              BuildCard(item: item[index], endPoint: widget.pageDetail.endPoint!),
-            ],
-          );
-        }
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          BuildTopicCard(
+            topic: weekStyle[0],
+            color: weekStyle[1],
+            margin: const EdgeInsets.all(8.0),
+          ),
+          for(int i=0;i<item.length;i++)
+            BuildCard(item: item[i], endPoint: widget.pageDetail.endPoint!),
+        ],
+      ),
     );
   }
 }
