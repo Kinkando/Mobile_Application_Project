1e8adbd71f8a0e6e2d3b3a2f52965d38231f5ff2
Tanawat Yuwansiri <TY_Sage@hotmail.com>
2021-11-11 15:19:26 +0700

    fix: reloaded error page bug

--- lib/pages/main/detail_page.dart
+++ lib/pages/main/detail_page.dart
@@ -1,7 +1,7 @@
 import 'package:anime_list_project/models/anime.dart';
 import 'package:anime_list_project/models/genre.dart';
 import 'package:anime_list_project/models/manga.dart';
-import 'package:anime_list_project/pages/home_page.dart';
+import 'package:anime_list_project/pages/main/home_page.dart';
 import 'package:anime_list_project/models/page_info.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
