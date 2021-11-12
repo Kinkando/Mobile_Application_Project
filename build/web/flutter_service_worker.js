be0f391e9e312b731954c3703361673edef0d464
Tanawat Yuwansiri <TY_Sage@hotmail.com>
2021-11-12 16:57:40 +0700

    change: splash screen image

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,10 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "41412289e2e0b43527a49a14337f07d7",
+  "assets/AssetManifest.json": "350b82fb33485634db24681f6919fb75",
 "assets/assets/images/bg.png": "ff7e0edba975fc4405168f7976e63b9d",
 "assets/assets/images/logo.png": "3d54b83064da347f588b95331111a337",
 "assets/assets/images/splash_screen.png": "d75c0f4b2dce2d7d8ec42f6ece4833ba",
+"assets/assets/images/splash_screen_v2.png": "d11fd5b355b7970600a886f21d56b641",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/NOTICES": "b190d1a83962890f013f8563eba1595a",
@@ -17,9 +18,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "046a5809fd16e6bdebc32b720a68220c",
-"/": "046a5809fd16e6bdebc32b720a68220c",
-"main.dart.js": "771fc838639fbf6986bbb53e30cc1e88",
+"index.html": "038842aa964c03b5f71cb68bb4743c4d",
+"/": "038842aa964c03b5f71cb68bb4743c4d",
+"main.dart.js": "38968fec120a5b9dc63e6e3ea020fe11",
 "manifest.json": "0b637ed19ebbd9f9a51ea484547de9f6",
 "version.json": "626f1e97171be407a60f9d5be63ad22d"
 };
